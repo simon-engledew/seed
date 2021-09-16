24fe4d677c859f82ea9ac64636a4fc77ab61439a
Simon Engledew <simon-engledew@github.com>
2021-09-16 16:56:15 +0100

    Working for 1

--- seed.go
+++ seed.go
@@ -1,115 +1,190 @@
 package seed
 
 import (
+	"context"
 	"fmt"
 	"github.com/brianvoe/gofakeit/v6"
-	"github.com/simon-engledew/seed/distribution"
 	"io"
-	"sort"
 	"strconv"
 	"strings"
 )
 
-type ColumnGenerator func() string
+type ColumnGenerator interface {
+	Value(ctx context.Context) string
+}
 
-type TableGenerator func(w io.Writer, tableName string) error
+type Dependent interface {
+	DependsOn() TableName
+}
 
-func Generate(schema map[string]TableGenerator) func(io.Writer) error {
-	return func(w io.Writer) error {
-		gofakeit.Seed(0)
+type TableName string
+type ColumnName string
+type Schema map[TableName]map[ColumnName]ColumnGenerator
 
-		for tableName, generator := range schema {
-			err := generator(w, tableName)
-			if err != nil {
-				return err
-			}
-		}
+type staticColumn struct {
+	value string
+}
 
-		return nil
+func (c *staticColumn) Value(ctx context.Context) string {
+	return c.value
+}
+
+func newStaticColumn(value string) ColumnGenerator {
+	return &staticColumn{value: value}
+}
+
+type callableColumn struct {
+	value func() string
+}
+
+func (c *callableColumn) Value(ctx context.Context) string {
+	return c.value()
+}
+
+func newCallableColumn(value func() string) ColumnGenerator {
+	return &callableColumn{value: value}
+}
+
+type contextKey string
+
+var parentKey contextKey = "parent"
+
+type dependentColumn struct {
+	tableName  TableName
+	columnName ColumnName
+}
+
+func (c *dependentColumn) DependsOn() TableName {
+	return c.tableName
+}
+
+func (c *dependentColumn) Value(ctx context.Context) string {
+	parent := ctx.Value(parentKey).(map[ColumnName]string)
+	return parent[c.columnName]
+}
+
+func Reference(tableName TableName, columnName ColumnName) ColumnGenerator {
+	return &dependentColumn{
+		tableName:  tableName,
+		columnName: columnName,
 	}
 }
 
-func Insert(dist distribution.Distribution, columns map[string]ColumnGenerator, next ...func(Reference) map[string]TableGenerator) TableGenerator {
-	return func(w io.Writer, tableName string) error {
-		columnNames := make([]string, 0, len(columns))
-		for columnName := range columns {
-			columnNames = append(columnNames, columnName)
-		}
+func Generate(w io.Writer, schema Schema) error {
+	gofakeit.Seed(0)
 
-		sort.Strings(columnNames)
+	graph := make(map[TableName]map[TableName]struct{})
 
-		count := dist()
-
-		for i := uint(0); i < count; i++ {
-			mapped := make(map[string]string, len(columns))
-
-			values := make([]string, 0, len(columnNames))
-			for _, columnName := range columnNames {
-				column := columns[columnName]
-				value := column()
-				mapped[columnName] = value
-				values = append(values, value)
-			}
-
-			ref := func(columnName string) ColumnGenerator {
-				return Identity(mapped[columnName])
-			}
-
-			_, err := fmt.Fprintf(w, "INSERT INTO %s (%s) VALUES (%s);\n", tableName, strings.Join(columnNames, ","), strings.Join(values, ","))
-			if err != nil {
-				return err
-			}
-
-			for _, fn := range next {
-				err := Generate(fn(ref))(w)
-				if err != nil {
-					return err
-				}
+	for tableName, columns := range schema {
+		graph[tableName] = make(map[TableName]struct{})
+		for _, generator := range columns {
+			if d, ok := generator.(Dependent); ok {
+				parent := d.DependsOn()
+				graph[tableName][parent] = struct{}{}
 			}
 		}
-
-		return nil
 	}
+
+	fmt.Println(graph)
+
+	order := make([][]TableName, 0)
+
+	for len(graph) > 0 {
+		var current []TableName
+		for table, dependents := range graph {
+			if len(dependents) == 0 {
+				current = append(current, table)
+				delete(graph, table)
+			}
+		}
+		for _, dependents := range graph {
+			for _, scheduled := range current {
+				delete(dependents, scheduled)
+			}
+		}
+		order = append(order, current)
+	}
+
+	for _, stage := range order {
+		fmt.Println(stage)
+	}
+
+	rows := make(map[TableName]map[ColumnName]string)
+
+	for _, stage := range order {
+		for _, table := range stage {
+			generators := schema[table]
+
+			row := make(map[ColumnName]string, len(generators))
+
+			rows[table] = row
+
+			for column, generator := range generators {
+				ctx := context.Background()
+
+				if d, ok := generator.(Dependent); ok {
+					parent := d.DependsOn()
+
+					ctx = context.WithValue(ctx, parentKey, rows[parent])
+				}
+
+				row[column] = generator.Value(ctx)
+			}
+
+		}
+	}
+
+	for table, _ := range schema {
+		_, err := insert(w, table, rows[table])
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func insert(w io.Writer, table TableName, row map[ColumnName]string) (int, error) {
+	columns := make([]string, 0, len(row))
+	values := make([]string, 0, len(row))
+
+	for column, value := range row {
+		columns = append(columns, string(column))
+		values = append(values, value)
+	}
+
+	return fmt.Fprintf(w, "INSERT INTO %s (%s) VALUES (%s);\n", table, strings.Join(columns, ","), strings.Join(values, ","))
 }
 
 func PrimaryKey() ColumnGenerator {
 	var id uint64
-	return func() string {
+
+	return newCallableColumn(func() string {
 		id += 1
 		return strconv.FormatUint(id, 10)
-	}
+	})
 }
 
-func Identity(val string) ColumnGenerator {
-	return func() string {
-		return val
-	}
-}
+var DateTime = newStaticColumn("NOW()")
 
-func DateTime() string {
-	return "NOW()"
-}
-
-func BigInt() string {
+var BigInt = newCallableColumn(func() string {
 	return gofakeit.DigitN(uint(gofakeit.Number(0, 10)))
-}
+})
 
-func GUID() string {
+var GUID = newCallableColumn(func() string {
 	return "'" + gofakeit.UUID() + "'"
-}
+})
 
 func String(size int) ColumnGenerator {
-	return func() string {
+	return newCallableColumn(func() string {
 		return "'" + gofakeit.LetterN(uint(gofakeit.Number(0, size))) + "'"
-	}
+	})
 }
 
-func Version() string {
+var Version = newCallableColumn(func() string {
 	return "'" + gofakeit.Generate("#.#.#") + "'"
-}
+})
 
-func TinyInt() string {
-	return gofakeit.DigitN(uint(gofakeit.Number(0, 10)))
-}
-
-type Reference func(columnName string) ColumnGenerator
+var TinyInt = newCallableColumn(func() string {
+	return gofakeit.DigitN(uint(gofakeit.Number(0, 1)))
+})
